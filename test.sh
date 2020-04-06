e2077e2808dde33debcd2e62fc1846295c306bca
Sanghyun Lee <hammerha@gmail.com>
2020-04-06 21:03:31 +0200

    Test only changed

--- test.sh
+++ test.sh
@@ -3,12 +3,13 @@
 # watch - https://github.com/mikeal/watch
 #
 
-filename="${1:-*.test.rkt}"
+# not used to test only changed
+#filename="${1:-*.test.rkt}"
 
 watchmedo shell-command \
           --patterns="*rkt" \
           --command="if [ \"\${watch_event_type}\" == \"created\" ];then \
                         echo '------------------------'; \
-                        raco test $filename; \
+                        raco test \"\${watch_src_path}\";
                     fi" \
 
