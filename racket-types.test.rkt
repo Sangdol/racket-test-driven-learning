1aea610436c9ed4e777500d7c472e3594f4efc7e
Sanghyun Lee <hammerha@gmail.com>
2020-04-06 23:10:19 +0200

    Add more type tests

--- racket-types.test.rkt
+++ racket-types.test.rkt
@@ -3,6 +3,11 @@
 (require rackunit)
 
 (test-case
+  "boolean"
+  (check-equal? #t #true)
+  (check-equal? #true true))
+
+(test-case
   "symbols"
   (check-true (symbol=? 'foo 'foo))
   (check-false (symbol=? 'foo 'Foo)))
@@ -31,5 +36,24 @@
   ; consing
   (check-equal? (cons 'a '(b c)) '(a b c)))
 
+(test-case
+  "list"
+  (check-equal? (list 'a 'b 'c) (cons 'a (cons 'b (cons 'c empty))))
+  (check-equal? (list 'a 'b 'c) '(a b c))
+  (check-equal? (first (rest '(a b c))) 'b)
+  (check-equal? (second (range 0 10)) 1)
+  (check-equal? (tenth (range 0 10)) 9))
 
+(test-case
+  "structure (usually for fixed number of items)"
+  (struct person (name age#))
+  (define sang (person 'Sang 36))
+  (check-equal? (person-name sang) 'Sang)  ; person-name: (field) selector
+  (check-equal? (person-age# sang) 36)
+  (check-true (person? sang))  ; person, huh?: type predicate
+  (check-false (struct? sang))  ; because it's opaque by default for encapulation
 
+  (struct naked (name age#) #:transparent)
+  (define n (naked 'n 0))
+  (check-true (naked? n))
+  (check-true (struct? n)))
