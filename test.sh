5fec6fdd4166cb8f4be857885b7efa0481dd96a6
Sanghyun Lee <hammerha@gmail.com>
2020-04-12 15:29:34 +0200

    Make tests time out

--- test.sh
+++ test.sh
@@ -6,10 +6,13 @@
 # not used to test only changed
 #filename="${1:-*.test.rkt}"
 
+# Timeout
+# https://stackoverflow.com/questions/687948/timeout-a-command-in-bash-without-unnecessary-delay
+
 watchmedo shell-command \
           --patterns="*rkt" \
           --command="if [ \"\${watch_event_type}\" == \"created\" ];then \
                         echo '------------------------'; \
-                        raco test \"\${watch_src_path}\";
+                        gtimeout 3 raco test \"\${watch_src_path}\"
                     fi" \
 
