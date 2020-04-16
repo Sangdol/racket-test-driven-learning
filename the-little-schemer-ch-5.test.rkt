4e97e9e4fef240999445c158566e4e1f9bea4b57
Sanghyun Lee <hammerha@gmail.com>
2020-04-17 01:32:44 +0200

    Add member*, leftmost, eqlist?

--- the-little-schemer-ch-5.test.rkt
+++ the-little-schemer-ch-5.test.rkt
@@ -89,5 +89,53 @@
   (check-equal? (insertL* 'a 'b '()) '())
   (check-equal? (insertL* 'a 'b '(b)) '(a b))
   (check-equal? (insertL* 'a 'b '((b))) '((a b)))
-  (check-equal? (insertL* 'a 'b '(c (b) a b)) '(c (a b) a a b)))
+  (check-equal? (insertL* 'a 'b '(c (b) a b)) '(c (a b) a a b))
 
+  (define member*
+    (lambda (a l)
+      (cond [(null? l) #f]
+            [(atom? (car l)) (or (eq? a (car l)) (member* a (cdr l)))]
+            [else (or (member* a (car l)) (member* a (cdr l)))])))
+
+  (check-false (member* 'a '()))
+  (check-false (member* 'a '(b)))
+  (check-false (member* 'a '((b))))
+  (check-true (member* 'a '(a)))
+  (check-true (member* 'a '((a))))
+  (check-true (member* 'a '((b a))))
+  (check-true (member* 'a '(b (b a))))
+
+  (define leftmost
+    (lambda (l)
+      (cond [(atom? (car l)) (car l)]
+            [else (leftmost (car l))])))
+
+  (check-equal? (leftmost '(a)) 'a)
+  (check-equal? (leftmost '((a))) 'a)
+  (check-equal? (leftmost '((a b))) 'a)
+
+  (define eqan?
+    (lambda (a b)
+      (cond [(and (number? a) (number? b)) (= a b)]
+            [(and (atom? a) (atom? b)) (eq? a b)]
+            [else #f])))
+
+  (define eqlist?
+    (lambda (l1 l2)
+      (cond [(and (null? l1) (null? l2)) #t]
+            [(or (null? l1) (null? l2)) #f]
+            [(and (atom? (car l1)) (atom? (car l2)))
+             (and (eqan? (car l1) (car l2))
+                  (eqlist? (cdr l1) (cdr l2)))]
+            [(or (atom? (car l1)) (atom? (car l2))) #f]
+            [else (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))])))
+
+  (check-false (eqlist? '(a) '(b)))
+  (check-false (eqlist? '(a a) '(b a)))
+  (check-false (eqlist? '(a a) '(b a)))
+  (check-false (eqlist? '(a a) '(a (a))))
+  (check-false (eqlist? '(a (a b)) '(a (a c))))
+  (check-true (eqan? 'a 'a))
+  (check-true (eqlist? '() '()))
+  (check-true (eqlist? '(a) '(a)))
+  (check-true (eqlist? '(a (a b)) '(a (a b)))))
