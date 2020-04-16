c2c709e87ac62e9937bf54037adb0d3ef2f503f8
Sanghyun Lee <hammerha@gmail.com>
2020-04-17 00:39:41 +0200

    Add occur*, subst* and insertL*

--- the-little-schemer-ch-5.test.rkt
+++ the-little-schemer-ch-5.test.rkt
@@ -50,5 +50,44 @@
   (check-equal? (insertR* 'a 'b '()) '())
   (check-equal? (insertR* 'a 'b '(b)) '(b a))
   (check-equal? (insertR* 'a 'b '((b))) '((b a)))
-  (check-equal? (insertR* 'a 'b '((b c) b d d)) '((b a c) b a d d)))
+  (check-equal? (insertR* 'a 'b '((b c) b d d)) '((b a c) b a d d))
+
+  (define occur*
+    (lambda (a l)
+      (cond [(null? l) 0]
+            [(atom? (car l))
+             (cond [(eq? a (car l)) (add1 (occur* a (cdr l)))]
+                   [else (occur* a (cdr l))])]
+            [else (+ (occur* a (car l)) (occur* a (cdr l)))])))
+
+  (check-equal? (occur* 'a '()) 0)
+  (check-equal? (occur* 'a '(a)) 1)
+  (check-equal? (occur* 'a '((a))) 1)
+  (check-equal? (occur* 'a '((a b) a b)) 2)
+
+  (define subst*
+    (lambda (new old l)
+      (cond [(null? l) l]
+            [(atom? (car l))
+             (cond [(eq? old (car l)) (cons new (subst* new old (cdr l)))]
+                   [else (cons (car l) (subst* new old (cdr l)))])]
+            [else (cons (subst* new old (car l)) (subst* new old (cdr l)))])))
+
+  (check-equal? (subst* 'a 'b '()) '())
+  (check-equal? (subst* 'a 'b '(b)) '(a))
+  (check-equal? (subst* 'a 'b '((b))) '((a)))
+  (check-equal? (subst* 'a 'b '(c (b c) b)) '(c (a c) a))
+
+  (define insertL*
+    (lambda (new old l)
+      (cond [(null? l) l]
+            [(atom? (car l))
+             (cond [(eq? old (car l)) (cons new (cons old (insertL* new old (cdr l))))]
+                   [else (cons (car l) (insertL* new old (cdr l)))])]
+            [else (cons (insertL* new old (car l)) (insertL* new old (cdr l)))])))
+
+  (check-equal? (insertL* 'a 'b '()) '())
+  (check-equal? (insertL* 'a 'b '(b)) '(a b))
+  (check-equal? (insertL* 'a 'b '((b))) '((a b)))
+  (check-equal? (insertL* 'a 'b '(c (b) a b)) '(c (a b) a a b)))
 
