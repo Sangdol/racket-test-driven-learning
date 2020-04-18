a91c283772d35b0de62d46c774c09ebf2c27866e
Sanghyun Lee <hammerha@gmail.com>
2020-04-18 22:31:02 +0200

    Add more Ch7

--- the-little-schemer-ch-7.test.rkt
+++ the-little-schemer-ch-7.test.rkt
@@ -17,6 +17,11 @@
           [(eq? a (car lat)) (multirember a (cdr lat))]
           [else (cons (car lat) (multirember a (cdr lat)))])))
 
+(define firsts
+  (lambda (l)
+    (cond [(null? l) l]
+          [else (cons (car (car l)) (firsts (cdr l)))])))
+
 (test-case
   "Ch7. Friends and Relations"
 
@@ -103,4 +108,128 @@
                       (intersect? (cdr set1) set2))])))
 
   (check-false (intersect? '(a) '(c b)))
-  (check-true (intersect? '(a) '(a b))))
+  (check-true (intersect? '(a) '(a b)))
+
+  (define intersect
+    (lambda (set1 set2)
+      (cond [(null? set1) '()]
+            [(member? (car set1) set2)
+             (cons (car set1) (intersect (cdr set1) set2))]
+            [else (intersect (cdr set1) set2)])))
+
+  (check-equal? (intersect '(a) '(b)) '())
+  (check-equal? (intersect '(a b) '(b)) '(b))
+  (check-equal? (intersect '(a b) '(b a)) '(a b))
+  (check-equal? (intersect '(a b c) '(b a)) '(a b))
+
+  (define union
+    (lambda (set1 set2)
+      (cond [(null? set1) set2]
+            [(member? (car set1) set2) (union (cdr set1) set2)]
+            [else (cons (car set1) (union (cdr set1) set2))])))
+
+  (check-equal? (union '() '()) '())
+  (check-equal? (union '(a) '()) '(a))
+  (check-equal? (union '(a) '(a)) '(a))
+  (check-equal? (union '(a b) '(a)) '(b a))
+
+  (define diffset
+    (lambda (set1 set2)
+      (cond [(null? set1) '()]
+            [(member? (car set1) set2) (diffset (cdr set1) set2)]
+            [else (cons (car set1) (diffset (cdr set1) set2))])))
+
+  (check-equal? (diffset '() '()) '())
+  (check-equal? (diffset '(a) '()) '(a))
+  (check-equal? (diffset '(a) '(a)) '())
+  (check-equal? (diffset '(a b) '(a)) '(b))
+
+  ; for non-empty set
+  ; l-set: (l)ist of (set)
+  (define intersectall
+    (lambda (l-set)
+      (cond [(null? (cdr l-set)) (car l-set)]
+            [else (intersect (car l-set)
+                             (intersectall (cdr l-set)))])))
+
+  (check-equal? (intersectall '((a) (b))) '())
+  (check-equal? (intersectall '((a b) (b))) '(b))
+  (check-equal? (intersectall '((a b) (b) (a b c))) '(b))
+
+  (define a-pair?
+    (lambda (x)
+      (and (not (null? x))
+           (not (null? (car x)))
+           (not (null? (cdr x)))
+           (null? (cdr (cdr x))))))
+
+  ; book version
+  (set! a-pair?
+    (lambda (x)
+      (cond [(atom? x) #f]              ; '(a)
+            [(null? x) #f]              ; '()
+            ; [(null? (cdr x)) #f]        ; '(a) - why this is needed?
+            [(null? (cdr (cdr x))) #t]  ; '(a b)
+            [else #f])))                ; '(a b c ...)
+
+  (check-false (a-pair? '()))
+  (check-false (a-pair? '(a b c)))
+  (check-true (a-pair? '(a b)))
+  (check-true (a-pair? '(a (b))))
+
+  (define (first p) (car p))
+  (define (second p) (car (cdr p)))
+  (define (third p) (car (cdr (cdr p))))
+
+  (check-equal? (first '(a b c)) 'a)
+  (check-equal? (second '(a b c)) 'b)
+  (check-equal? (third '(a b c)) 'c)
+
+  (define (build s1 s2) (cons s1 (cons s2 '())))
+
+  (check-equal? (build 'a 'b) '(a b))
+
+  ; rel: set of pairs (relation)
+  ; fun: function - what does 'function' has to do with this function?
+  ; finite function: a list of pairs in which no first element of any pair is the same
+  ;                  as any other first element - why?
+  (define fun?
+    (lambda (rel)
+      (set? (firsts rel))))
+
+  (check-false (fun? '((a b) (a c))))
+  (check-true (fun? '((a b) (e c))))
+
+  ; (rev)erse (rel)ation
+  (define revrel
+    (lambda (rel)
+      (cond [(null? rel) '()]
+            [else (cons (build (second (car rel)) (first (car rel)))
+                        (revrel (cdr rel)))])))
+
+  (check-equal? (revrel '((a b) (e c))) '((b a) (c e)))
+
+  (define revpair
+    (lambda (pair)
+      (build (second pair) (first pair))))
+
+  (check-equal? (revpair '(a b)) '(b a))
+
+  (set! revrel
+    (lambda (rel)
+      (cond [(null? rel) '()]
+            [else (cons (revpair (car rel)) (revrel (cdr rel)))])))
+
+  (check-equal? (revrel '((a b) (e c))) '((b a) (c e)))
+
+  (define seconds
+    (lambda (l)
+      (cond [(null? l) l]
+            [else (cons (car (cdr (car l))) (seconds (cdr l)))])))
+
+  (define fullfun?
+    (lambda (fun)
+      (and (set? (seconds fun)))))
+
+  (check-false (fullfun? '((a b) (e b))))
+  (check-true (fullfun? '((a b) (e c)))))
