254b096f96ac836d009361047a338dd40b4acb2c
Sanghyun Lee <hammerha@gmail.com>
2020-11-15 22:11:39 +0100

    Update description

--- test.sh
+++ test.sh
@@ -2,13 +2,11 @@
 #
 # watch - https://github.com/mikeal/watch
 #
-
-# not used to test only changed
-#filename="${1:-*.test.rkt}"
-
 # Timeout
 # https://stackoverflow.com/questions/687948/timeout-a-command-in-bash-without-unnecessary-delay
-
+#
+# Run test when a file is updated by vim.
+# It has a timeout to stop an infinite loop.
 watchmedo shell-command \
           --patterns="*rkt" \
           --command="if [ \"\${watch_event_type}\" == \"created\" ];then \
